15f9c8271f4740435b339ba6fed6ff6658392653
Corentin-tin <merle.corentin@yahoo.com>
2022-10-11 13:09:17 +0200

    some not important changes

--- AirQuality-EDA.R
+++ AirQuality-EDA.R
@@ -1,17 +1,17 @@
-air_quality_datat<-read.table("AirQuality.csv",header=TRUE,sep=";",stringsAsFactors=TRUE)
-attach(air_quality_datat)
+air_quality_data<-read.table("AirQuality.csv",header=TRUE,sep=";",stringsAsFactors=TRUE)
+attach(air_quality_data)
 
 dim(air_quality_datat)
 
 # Select the 500 random samples
 air_quality_datat<-air_quality_datat[sample(nrow(air_quality_datat), 500), ]
-dim(air_quality_datat)
+dim(air_quality_data)
 
-summary(air_quality_datat)
+summary(air_quality_data)
 
 # Remove column x & x.1
-air_quality_datat <- subset (air_quality_datat, select = -X)
-air_quality_datat <- subset (air_quality_datat, select = -X.1)
+air_quality_datat <- subset (air_quality_data, select = -X)
+air_quality_datat <- subset (air_quality_data, select = -X.1)
 
 # "Missing values are tagged with -200 value" => -200<-NA
 air_quality_datat[air_quality_datat == -200]<- NA
@@ -20,14 +20,14 @@ summary(air_quality_datat)
 
 # Vizualize missing values
 library(visdat)
-vis_miss(air_quality_datat)
-vis_dat(air_quality_datat)
-gg_miss_upset(air_quality_datat)
+vis_miss(air_quality_data)
+vis_dat(air_quality_data)
+gg_miss_upset(air_quality_data)
 
 library(ggplot2)
 library(naniar)
-ggplot(air_quality_datat,aes(x=AH,y=RH))+ geom_miss_point()
-ggplot(data,aes(x=Height,y=Loss))+ geom_miss_point()
+ggplot(air_quality_data,aes(x=AH,y=RH))+ geom_miss_point()
+
 
 
 detach(air_quality_datat)
