c20454bc5674c6f5cec7ae8367fa6dbde20fcdac
JadAkkawi <jad.akkawi@hotmail.com>
2022-10-16 12:40:14 +0200

    added some analysis about the reasons for missing data

--- missing_data.R
+++ missing_data.R
@@ -16,3 +16,16 @@ vis_dat(air_quality_data)
 gg_miss_upset(air_quality_data)
 
 ggplot(air_quality_data,aes(x=NMHC.GT.,y=C6H6.GT.)) + geom_miss_point() 
+
+## We can see from the plot that most missing values are "non metanic hydrocarbure" 103/133 cases 
+## NOx and NO2  are missing together 22/133 times
+## CO is missing 18/133 times
+
+## if we plot the observations where NO2 and NOX are abscent 
+air_quality_data[which(is.na(NO2.GT.)),]
+## we realize that during the month of march at 3:00 AM the reference center 
+## did not  measure any data regarding NO2 and NOx maybe because the machines were resetting
+
+# plotting NMHC missing values for inferring reasons
+air_quality_data[which(is.na(NMHC.GT.)),]
+## we realize that from 18th till 20th of Mars these elements weren't measured
