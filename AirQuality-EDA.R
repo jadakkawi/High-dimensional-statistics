ff7d1bd9d3281328033a5a15b53ce1e4b044440f
JadAkkawi <jad.akkawi@hotmail.com>
2022-10-16 12:40:45 +0200

    removed the parts that were done in other R files

--- AirQuality-EDA.R
+++ AirQuality-EDA.R
@@ -1,32 +1,10 @@
-library(ggplot2)
-library(naniar)
-library(visdat)
-library(moments)
-library(corrplot)
-
-#air_quality_data <- read.table("AirQuality.csv",header=TRUE,sep=";",stringsAsFactors=TRUE)
-air_quality_data <- read.table('AirQuality.csv', dec=",", sep=";" , header=TRUE, stringsAsFactors=TRUE)
+air_quality_data <- read.table('preProcessAirQuality.csv', sep=";" , 
+                               header=TRUE, stringsAsFactors=TRUE)
 attach(air_quality_data)
-
-dim(air_quality_data)
-
-# Select the 500 random samples
-air_quality_data<-air_quality_data[sample(nrow(air_quality_data), 500), ]
-dim(air_quality_data)
-
-summary(air_quality_data)
-
-# Remove column x & x.1
-air_quality_data <- subset (air_quality_data, select = -X)
-air_quality_data <- subset (air_quality_data, select = -X.1)
-
-# "Missing values are tagged with -200 value" => -200<-NA
-air_quality_data[air_quality_data == -200]<- NA
 
 dim(air_quality_data)
 summary(air_quality_data)
 
-attach(air_quality_data)
 
 #apply(apply(air_quality_data, 2, gsub, patt=",", replace="."), 2, as.numeric)
 #gsub(",", ".",air_quality_data[str_contains(air_quality_data,",")])
@@ -34,11 +12,7 @@ attach(air_quality_data)
 #air_quality_data[air_quality_data == '']<- NA
 #air_quality_data <- replace(air_quality_data, air_quality_data=='', NA)
 
-# Vizualize missing values
-vis_miss(air_quality_data)
-vis_dat(air_quality_data)
-gg_miss_upset(air_quality_data)
-ggplot(air_quality_data,aes(x=NMHC.GT.,y=C6H6.GT.)) + geom_miss_point() 
+ 
 
 # -----------------------------------------
 
