ca118e91f8153b056f630afe349ec949b5615942
Corentin-tin <merle.corentin@yahoo.com>
2022-10-18 15:39:40 +0200

    cocommit

--- EDA.R
+++ EDA.R
@@ -2,14 +2,84 @@
 # University of Liège
 # Academic year 2022-2023
 # Project : Exploratory data analysis
-# File : ???????????????????????????????????????????????
+# File : EDA.R
 # Authors :
 #     - Merle Corentin 
 #     - Jad Akkawi
-library(visdat)
 
-air_quality_data <- read.table('cleanAirQuality.csv', sep=";" , 
-                               header=TRUE, stringsAsFactors=TRUE)
+library(visdat) 
+library(MASS)
+
+plotAndSaveHistNorm <- function(x, name=name) {
+  
+  x = as.numeric(unlist(x))
+  
+  hist = hist(x, col="darkgreen", freq=FALSE, breaks=20,
+              main = paste("Histogram of", name), xlab=name)
+  x_values <- seq(min(na.omit(x)), max(na.omit(x)), length = 100)
+  y_values <- dnorm(x_values, mean = mean(na.omit(x)), sd = sd(na.omit(x))) 
+  #y_values <- y_values * diff(hist$mids[1:2]) * length(x) 
+  
+  lines(x_values, y_values, lwd = 3, col="green")
+  
+  dev.copy(pdf, paste("Plots/hist_normal_",name,".pdf"), width=20, height=11)
+  dev.off()
+  
+  print(paste(name, " : MLE = ", BIC(fitdistr(na.omit(x), densfun="normal"))))
+  
+}
+
+plotAndSaveHistPoisson <- function(x, name=name) {
+  
+  x = as.numeric(unlist(x))
+  
+  hist = hist(x, col="darkgreen", freq=FALSE, breaks=20,
+              main = paste("Histogram of", name), xlab=name)
+  x_values <- seq(floor(min(na.omit(x))), ceiling(max(na.omit(x))))
+  
+  estimate_param = fitdistr(na.omit(x), densfun="poisson")
+  print(paste(name, " : MLE = ", BIC(estimate_param)))
+  estimate_param = unlist(estimate_param)
+  
+  y_values <- dpois(x_values, 
+                    lambda=estimate_param["estimate.lambda"]) 
+  
+  lines(x_values, y_values, lwd = 3, col="green")
+  
+  dev.copy(pdf, paste("Plots/hist_poisson_", name, ".pdf"), width=20, height=11) 
+  dev.off()
+  
+  
+  
+}
+
+plotAndSaveHistLogNormal <- function(x, name=name) {
+  
+  x = as.numeric(unlist(x))
+  
+  hist = hist(x, col="darkgreen", freq=FALSE, breaks=20,
+              main = paste("Histogram of", name), xlab=name)
+  x_values <- seq(min(na.omit(x)), max(na.omit(x)), length = 100)
+  
+  estimate_param = fitdistr(na.omit(x), densfun="lognormal")
+  print(paste(name, " : MLE = ", BIC(estimate_param)))
+  estimate_param = unlist(estimate_param)
+  
+  y_values <- dlnorm(x_values, 
+                     meanlog = estimate_param["estimate.meanlog"],
+                     sdlog = estimate_param["estimate.sdlog"], 
+                     log = FALSE) 
+  
+  lines(x_values, y_values, lwd = 3, col="green")
+  
+  dev.copy(pdf, paste("Plots/hist_logNormal_", name, ".pdf"), width=20, height=11) 
+  dev.off()
+  
+  
+  
+}
+
+attach(air_quality_data)
 
 summary(air_quality_data)
 dim(air_quality_data)
@@ -18,107 +88,37 @@ vis_miss(air_quality_data)
 
 # Historgrams + Normal 
 
-## CO
+for (name in names(air_quality_data)){
+  plotAndSaveHistNorm(air_quality_data[name], name=name)
+}
 
-hist_imputed = hist(Imputed_CO, col="blue2", 
-                    main = paste("CO.GT. & imputed CO.GT. histogram"), xlab="CO")
-hist = hist(CO.GT., col="darkgreen", add=TRUE)
+# Historgrams + Poisson
 
-x_values_imputed <- seq(min(Imputed_CO), max(Imputed_CO), length = 100)
-y_values_imputed <- dnorm(x_values_imputed, mean = mean(Imputed_CO), sd = sd(Imputed_CO)) 
-y_values_imputed <- y_values_imputed * diff(hist_imputed$mids[1:2]) * length(Imputed_CO) 
+for (name in names(air_quality_data)){
+  plotAndSaveHistPoisson(air_quality_data[name], name=name)
+}
 
-lines(x_values_imputed, y_values_imputed, lwd = 3, col="green")
+# Historgrams + Log Normal
 
-x_values <- seq(min(na.omit(CO.GT.)), max(na.omit(CO.GT.)), length = 100)
-y_values <- dnorm(x_values, mean = mean(na.omit(CO.GT.)), sd = sd(na.omit(CO.GT.))) 
-y_values <- y_values * diff(hist$mids[1:2]) * length(CO.GT.) 
+for (name in names(air_quality_data)){
+  plotAndSaveHistLogNormal(air_quality_data[name], name=name)
+}
 
-lines(x_values, y_values, lwd = 3, col="blue")
+detach(air_quality_data)
 
-hist(CO.GT., freq=FALSE)
-lines(density(na.omit(CO.GT.), bw=0.8), col="red")
-(my.mle<-fitdistr(na.omit(CO.GT.), densfun="poisson"))
-##      lambda  
-##   20.6700000 
-##  ( 0.4546427)
-BIC(my.mle)
+# Date -> uniform
+# Time -> uniform
+# CO.GT. -> poisson
+# PT08.S1.CO. -> log normal
+# NMHC.GT.-> log normal
+# C6H6.GT. -> log normal
+# PT08.S2.NMHC.-> log normal
+# NOx.GT. -> log normal 
+# PT08.S3.NOx. -> log normal
+# NO2.GT. -> normal
+# PT08.S4.NO2. -> log normal
+# PT08.S5.O3. -> log normal
+# T -> log normal
+# RH -> ?
+# AH -> ?
 
-## NMHC
-
-hist_imputed = hist(Imputed_NMHC, col="blue2", 
-                    main = paste("NMHC.GT. & imputed NMHC.GT. histogram"), xlab="CO")
-hist = hist(NMHC.GT., col="darkgreen", add=TRUE)
-
-x_values_imputed <- seq(min(Imputed_NMHC), max(Imputed_NMHC), length = 100)
-y_values_imputed <- dnorm(x_values_imputed, mean = mean(Imputed_NMHC), sd = sd(Imputed_NMHC)) 
-y_values_imputed <- y_values_imputed * diff(hist_imputed$mids[1:2]) * length(Imputed_NMHC) 
-
-lines(x_values_imputed, y_values_imputed, lwd = 3, col="green")
-
-x_values <- seq(min(na.omit(NMHC.GT.)), max(na.omit(NMHC.GT.)), length = 100)
-y_values <- dnorm(x_values, mean = mean(na.omit(NMHC.GT.)), sd = sd(na.omit(NMHC.GT.))) 
-y_values <- y_values * diff(hist$mids[1:2]) * length(NMHC.GT.) 
-
-lines(x_values, y_values, lwd = 3, col="blue")
-
-## NOx
-
-hist_imputed = hist(Imputed_NOx, col="blue2", 
-                    main = paste("NOx.GT. & imputed NOx.GT. histogram"), xlab="NOx")
-hist = hist(NOx.GT., col="darkgreen", add=TRUE)
-
-x_values_imputed <- seq(min(Imputed_NOx), max(Imputed_NOx), length = 100)
-y_values_imputed <- dnorm(x_values_imputed, mean = mean(Imputed_NOx), sd = sd(Imputed_NOx)) 
-y_values_imputed <- y_values_imputed * diff(hist_imputed$mids[1:2]) * length(Imputed_NOx) 
-
-lines(x_values_imputed, y_values_imputed, lwd = 3, col="green")
-
-x_values <- seq(min(na.omit(NOx.GT.)), max(na.omit(NOx.GT.)), length = 100)
-y_values <- dnorm(x_values, mean = mean(na.omit(NOx.GT.)), sd = sd(na.omit(NOx.GT.))) 
-y_values <- y_values * diff(hist$mids[1:2]) * length(NOx.GT.) 
-
-lines(x_values, y_values, lwd = 3, col="blue")
-
-## NO2
-
-hist_imputed = hist(Imputed_NO2, col="blue2", 
-                    main = paste("NO2.GT. & imputed NO2.GT. histogram"), xlab="NO2")
-hist = hist(NO2.GT., col="darkgreen", add=TRUE)
-
-x_values_imputed <- seq(min(Imputed_NO2), max(Imputed_NO2), length = 100)
-y_values_imputed <- dnorm(x_values_imputed, mean = mean(Imputed_NO2), sd = sd(Imputed_NO2)) 
-y_values_imputed <- y_values_imputed * diff(hist_imputed$mids[1:2]) * length(Imputed_NO2) 
-
-lines(x_values_imputed, y_values_imputed, lwd = 3, col="green")
-
-x_values <- seq(min(na.omit(NO2.GT.)), max(na.omit(NO2.GT.)), length = 100)
-y_values <- dnorm(x_values, mean = mean(na.omit(NO2.GT.)), sd = sd(na.omit(NO2.GT.))) 
-y_values <- y_values * diff(hist$mids[1:2]) * length(NO2.GT.) 
-
-lines(x_values, y_values, lwd = 3, col="blue")
-
-# Histograms + Poisson 
-
-## CO
-
-hist(CO.GT., freq=FALSE, breaks=100)
-lines(density(na.omit(CO.GT.), bw=0.8), col="red")
-(my.mle<-fitdistr(na.omit(CO.GT.), densfun="poisson"))
-##      lambda  
-##   20.6700000 
-##  ( 0.4546427)
-BIC(my.mle)
-
-# 
-# hist(Imputed_NMHC, col="blue2", 
-#      main = paste("NMHC.GT. & imputed NMHC.GT. histogram"), xlab="NMHC")
-# hist(NMHC.GT., col="darkgreen", add=TRUE)
-# 
-# hist(Imputed_NOx, col="blue2", 
-#      main = paste("NOx.GT. & imputed NOx.GT. histogram"), xlab="NOx")
-# hist(NOx.GT., col="darkgreen", add=TRUE)
-# 
-# hist(Imputed_NO2, col="blue2", 
-#      main = paste("NO2.GT. & imputed NO2.GT. histogram"), xlab="NO2")
-# hist(NO2.GT., col="darkgreen", add=TRUE)
